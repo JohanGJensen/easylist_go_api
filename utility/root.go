aa9c8d99d0ead0b5504c0e34dcc3826d65f50c25
JohanGJensen <johancentralen@gmail.com>
2023-09-09 14:04:38 +0200

    remove app from file title

--- utility/root.go
+++ utility/root.go
@@ -11,7 +11,7 @@ type Config struct {
 
 func LoadConfig(path string) (config Config, err error) {
 	viper.AddConfigPath(path)
-	viper.SetConfigName("app")
+	viper.SetConfigName(".env")
 	viper.SetConfigType("env")
 
 	viper.AutomaticEnv()
